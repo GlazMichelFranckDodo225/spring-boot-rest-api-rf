295035947cef1aae3150a85f66f77348e0c3f3ba
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-27 04:27:21 +0200

    REST API Endpoint @GetMapping("/hello-world")

--- src/main/java/com/dgmf/controller/HelloWorldController.java
+++ src/main/java/com/dgmf/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.dgmf.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // @Controller ==> Makes this Class as "Spring MVC" Controller
@@ -7,4 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 // into JSON Object and passed back into the HttpResponse Object
 @RestController // Combination of "@Controller" and "@ResponseBody" Annotations
 public class HelloWorldController {
+    // Http Get Request
+    // Map Http Get Request onto specific Handler Method
+    @GetMapping("/hello-world") // http://localhost:8080/hello-world
+    public String helloWorld() {
+        return "Hello world !";
+    }
 }
