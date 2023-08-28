fe01c0a3cde1aae6871ae655aedf0dd62783b883
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-28 08:01:15 +0200

    Setting up the REST API Endpoint "@GetMapping("/students")"

--- src/main/java/com/dgmf/controller/StudentController.java
+++ src/main/java/com/dgmf/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.dgmf.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("/student") // http://localhost:8080/student
@@ -15,5 +18,17 @@ public class StudentController {
                 .build();
 
         return student;
+    }
+
+    @GetMapping("/students") // http://localhost:8080/students
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student(1L, "John", "Doe"));
+        students.add(new Student(2L, "Alex", "Fonzy"));
+        students.add(new Student(3L, "Laure", "Esteban"));
+        students.add(new Student(4L, "Ynolla", "Orson"));
+
+        return students;
     }
 }
