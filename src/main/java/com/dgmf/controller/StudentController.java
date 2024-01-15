551b31e4c1a6b6ced83a61543cdedcb6ad9e8a97
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-15 06:04:21 +0100

    Using Spring ResponseEntity to Manipulate the HTTP Response

--- src/main/java/com/dgmf/controller/StudentController.java
+++ src/main/java/com/dgmf/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.dgmf.controller;
 
 import com.dgmf.dto.StudentDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,18 +11,22 @@ import java.util.List;
 @RestController
 public class StudentController {
     @GetMapping("/student") // http://localhost:8080/student
-    public StudentDTO getStudent() {
+    public ResponseEntity<StudentDTO> getStudent() {
         StudentDTO studentDTO = StudentDTO.builder()
                 .id(1L)
                 .firstName("John")
                 .lastName("Doe")
                 .build();
 
-        return studentDTO;
+        // return new ResponseEntity<>(studentDTO, HttpStatus.OK);
+        // return ResponseEntity.ok(studentDTO);
+        return ResponseEntity.ok() // Create a Builder with the Status Set to OK
+                .header("custom-header", "John Doe")
+                .body(studentDTO);
     }
 
     @GetMapping("/students") // http://localhost:8080/students
-    public List<StudentDTO> getStudents() {
+    public ResponseEntity<List<StudentDTO>> getStudents() {
         List<StudentDTO> studentDTOS = new ArrayList<>();
 
         studentDTOS.add(new StudentDTO(1L, "John", "Doe"));
@@ -29,13 +34,13 @@ public class StudentController {
         studentDTOS.add(new StudentDTO(3L, "Laure", "Esteban"));
         studentDTOS.add(new StudentDTO(4L, "Ynolla", "Orson"));
 
-        return studentDTOS;
+        return ResponseEntity.ok(studentDTOS);
     }
 
     @GetMapping("/students/{id}/{first-name}/{last-name}")
     // "id", "fn" and "ln" ==> URI Template variables
     // http://localhost:8080/students/1/John/Doe
-    public StudentDTO studentPathVariable(
+    public ResponseEntity<StudentDTO> studentPathVariable(
             @PathVariable("id") Long studentId,
             @PathVariable("first-name") String studentFirstName,
             @PathVariable("last-name") String studentLastName
@@ -46,13 +51,13 @@ public class StudentController {
                 .lastName(studentLastName)
                 .build();
 
-        return studentDTO;
+        return ResponseEntity.ok(studentDTO);
     }
 
     @GetMapping("/students/query") // REST API Endpoint should be unique
     // "id", "firstname" and "lastname" ==> Query Parameters (Query Strings)
     // http://localhost:8080/students/query?id=1&firstname=John&lastname=Doe
-    public StudentDTO studentRequestVariable(
+    public ResponseEntity<StudentDTO> studentRequestVariable(
             @RequestParam("id") Long studentId,
             @RequestParam("firstname") String studentFirstName,
             @RequestParam("lastname") String studentLastName
@@ -63,25 +68,27 @@ public class StudentController {
                 .lastName(studentLastName)
                 .build();
 
-        return studentDTO;
+        return ResponseEntity.ok(studentDTO);
     }
 
     // Rest API that handles HTTP POST Request - Creating new resource
     @PostMapping("/students/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public StudentDTO createStudent(@RequestBody StudentDTO studentDTO) {
+    // @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<StudentDTO> createStudent(
+            @RequestBody StudentDTO studentDTO
+    ) {
         StudentDTO student = StudentDTO.builder()
                 .id(studentDTO.getId())
                 .firstName(studentDTO.getFirstName())
                 .lastName(studentDTO.getLastName())
                 .build();
 
-        return student;
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     // Rest API that handles HTTP POST Request - Updating existing resource
     @PutMapping("/students/{id}/update")
-    public StudentDTO updateStudent(
+    public ResponseEntity<StudentDTO> updateStudent(
             @RequestBody StudentDTO studentDTO,
             @PathVariable("id") Long studentId
     ) {
@@ -91,13 +98,14 @@ public class StudentController {
                 .lastName(studentDTO.getLastName())
                 .build();
 
-        return student;
+        return ResponseEntity.ok(student);
     }
 
     // Rest API that handles HTTP DELETE Request - Deleting existing resource
     @DeleteMapping("/students/{id}/delete")
-    public String deleteStudent(@PathVariable("id") Long studentId) {
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") Long studentId) {
         System.out.println(studentId);
-        return "Student Deleted Successfully.";
+
+        return ResponseEntity.ok("Student Deleted Successfully.");
     }
 }
